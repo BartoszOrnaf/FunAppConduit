a2a352b783e824b4f7b6a7546d23655ef0bb9c19
Ornaf <bartosz.ornaf@gmail.com>
2019-07-10 10:14:06 +0200

    First version of the app

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,43 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+let tags = {};
+
+// $(document).ready(function () {
+//     $.ajax({
+//         url: "https://conduit.productionready.io/api/tags",
+//         dataType: "json"
+//     })
+//         .done(function (data) {
+//             tags = data;
+//             printTags();
+//             console.log(tags);
+//         });
+// });
+
+fetch("https://conduit.productionready.io/api/tags")
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (data) {
+        return JSON.stringify(data);
+    })
+    .then(function (data) {
+        tags = JSON.parse(data);
+        printTags();
+    }).catch(function(){
+        alert('Wrong URL');
+    });
+
+function printTags() {
+    let htmlContentTags = '';
+    if (tags) {
+        for (let i = 0; i < tags.tags.length; i++) {
+            htmlContentTags = htmlContentTags + `<button type="button" class="btn btn-secondary btn-sm">${tags.tags[i]}</button> `;
+        };
+        $('.tabsClass').html(function () {
+            return `<div>Popular tags: <br>${htmlContentTags}</div>`;
+        });
+    }
+}
+
+
