a57b615b5b240e20e0a350086cab3796c8b9d7d1
Ornaf <bartosz.ornaf@gmail.com>
2019-07-23 20:08:04 +0200

    Display by jquery objects

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import '../styles/index.scss';
 
 let tags = {};
+let articles = {};
 
 // $(document).ready(function () {
 //     $.ajax({
@@ -9,10 +10,23 @@ let tags = {};
 //     })
 //         .done(function (data) {
 //             tags = data;
-//             printTags();
-//             console.log(tags);
+//             printTags();//           
 //         });
 // });
+
+fetch("https://conduit.productionready.io/api/articles")
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (data) {
+        return JSON.stringify(data);
+    })
+    .then(function (data) {
+        articles = JSON.parse(data);
+        printArticles();
+    }).catch(function () {
+        alert('Loading articles problem');
+    });
 
 fetch("https://conduit.productionready.io/api/tags")
     .then(function (data) {
@@ -24,7 +38,7 @@ fetch("https://conduit.productionready.io/api/tags")
     .then(function (data) {
         tags = JSON.parse(data);
         printTags();
-    }).catch(function(){
+    }).catch(function () {
         alert('Wrong URL');
     });
 
@@ -40,4 +54,71 @@ function printTags() {
     }
 }
 
+function printArticles() {
+    if (articles) {
+        $('#articles').append(
+            createHtml()
+        );
+    }
+}
+
+function createHtml() {
+    let htmlResult = $('<div></div>');
+
+    articles.articles.forEach(element => {
+        let userThumbnail = $('<img/>', {
+            class: "userImage",
+            src: element.author.image
+        });
+        let userName = $(`<p>${element.author.username}</p>`).addClass("userName");
+        let shortDate = element.createdAt.slice(0, 10);
+        let postedOn = $(`<p>${shortDate}</p>`).addClass("postedOn");
+        let favoritesCount = element.favoritesCount;
+        let articleTitle = $(`<h1>${element.title}</h1>`);
+        let articleDescription = $(`<p>${element.description}</p>`);
+        let readMore = $('<a/>', {
+            text: "Read more..."
+        }).prop("href", `https://demo.realworld.io/#/article/${element.slug}`);
+
+        htmlResult.append(
+            '<hr>'
+        ).append(
+            $('<div/>', {
+                class: 'feedContainer'
+            }).append(
+                $('<div/>').append(
+                    userThumbnail
+                )).append(
+                    $('<div/>').append(
+                        userName
+                    ).append(
+                        postedOn
+                    )).append(
+                        $('<div/>', {
+                            style: "text-align:right"
+                        }).append(
+                            $('<button/>', {
+                                type: "button",
+                                class: "btn btn-outline-success",
+                                text: `❤ ${favoritesCount}`
+                            }).click(function () {
+                                alert('Adding likes is not working jet!');
+                            })
+                        )
+                    )
+        ).append(
+            $('<br>')
+        ).append(
+            articleTitle
+        ).append(
+            articleDescription
+        ).append(
+            $(`<span></span>`).append(
+                readMore
+            )
+        );
+    });
+
+    return htmlResult;
+}
 
