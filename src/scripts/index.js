d2747b9725e6ed47d2f34c615d91e2d03b1be372
Ornaf <bartosz.ornaf@gmail.com>
2019-07-29 18:19:29 +0200

    Splitting code to smaller chunks and variables in SASS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,122 +3,118 @@ import '../styles/index.scss';
 let tags = {};
 let articles = {};
 
-// $(document).ready(function () {
-//     $.ajax({
-//         url: "https://conduit.productionready.io/api/tags",
-//         dataType: "json"
-//     })
-//         .done(function (data) {
-//             tags = data;
-//             printTags();//           
-//         });
-// });
-
-fetch("https://conduit.productionready.io/api/articles")
-    .then(function (data) {
-        return data.json();
-    })
-    .then(function (data) {
-        return JSON.stringify(data);
-    })
-    .then(function (data) {
-        articles = JSON.parse(data);
-        printArticles();
-    }).catch(function () {
-        alert('Loading articles problem');
-    });
-
-fetch("https://conduit.productionready.io/api/tags")
-    .then(function (data) {
-        return data.json();
-    })
-    .then(function (data) {
-        return JSON.stringify(data);
-    })
-    .then(function (data) {
-        tags = JSON.parse(data);
-        printTags();
-    }).catch(function () {
-        alert('Wrong URL');
-    });
-
-function printTags() {
-    let htmlContentTags = '';
-    if (tags) {
-        for (let i = 0; i < tags.tags.length; i++) {
-            htmlContentTags = htmlContentTags + `<button type="button" class="btn btn-secondary btn-sm">${tags.tags[i]}</button> `;
+function getData(url) {
+    return new Promise((resolve, reject) => {
+        var httpReq = new XMLHttpRequest();
+        httpReq.onreadystatechange = () => {
+            if (httpReq.readyState === 4) {
+                if (httpReq.status === 200) {
+                    resolve(JSON.parse(httpReq.responseText));
+                } else {
+                    reject(new Error(httpReq.statusText));
+                }
+            }
         };
-        $('.tabsClass').html(function () {
+        httpReq.open("GET", url, true);
+        httpReq.send();
+    });
+}
+
+getData("https://conduit.productionready.io/api/articles").then(function (data) {
+    articles = data;
+    printArticles(articles, '#articles');
+});
+
+getData("https://conduit.productionready.io/api/tags").then(function (data) {
+    tags = data;
+    printTags(tags, '.tabs-class');
+});
+
+function printTags(input, selector) {
+    let htmlContentTags = '';
+    if (input) {
+        for (let i = 0; i < input.tags.length; i++) {
+            htmlContentTags = htmlContentTags + `<button type="button" class="btn btn-secondary btn-sm">${input.tags[i]}</button> `;
+        };
+        $(selector).html(function () {
             return `<div>Popular tags: <br>${htmlContentTags}</div>`;
         });
     }
-}
+};
 
-function printArticles() {
-    if (articles) {
-        $('#articles').append(
+function printArticles(input, selector) {
+    if (input) {
+        $(selector).append(
             createHtml()
         );
-    }
-}
+    };
+};
 
 function createHtml() {
     let htmlResult = $('<div></div>');
 
-    articles.articles.forEach(element => {
-        let userThumbnail = $('<img/>', {
-            class: "userImage",
-            src: element.author.image
-        });
-        let userName = $(`<p>${element.author.username}</p>`).addClass("userName");
-        let shortDate = element.createdAt.slice(0, 10);
-        let postedOn = $(`<p>${shortDate}</p>`).addClass("postedOn");
-        let favoritesCount = element.favoritesCount;
-        let articleTitle = $(`<h1>${element.title}</h1>`);
-        let articleDescription = $(`<p>${element.description}</p>`);
-        let readMore = $('<a/>', {
-            text: "Read more..."
-        }).prop("href", `https://demo.realworld.io/#/article/${element.slug}`);
-
-        htmlResult.append(
-            '<hr>'
-        ).append(
-            $('<div/>', {
-                class: 'feedContainer'
-            }).append(
-                $('<div/>').append(
-                    userThumbnail
-                )).append(
-                    $('<div/>').append(
-                        userName
-                    ).append(
-                        postedOn
-                    )).append(
-                        $('<div/>', {
-                            style: "text-align:right"
-                        }).append(
-                            $('<button/>', {
-                                type: "button",
-                                class: "btn btn-outline-success",
-                                text: `❤ ${favoritesCount}`
-                            }).click(function () {
-                                alert('Adding likes is not working jet!');
-                            })
-                        )
-                    )
-        ).append(
-            $('<br>')
-        ).append(
-            articleTitle
-        ).append(
-            articleDescription
-        ).append(
-            $(`<span></span>`).append(
-                readMore
-            )
-        );
+    articles.articles.forEach(articleObj => {
+        htmlResult.append(createArticleHTMLPartial(articleObj));
     });
 
     return htmlResult;
-}
+};
+
+function createArticleHTMLPartial(articleObj) {
+    let articleHTML = $('<div></div>');
+
+    let userThumbnail = $('<img/>', {
+        class: "user-image",
+        src: articleObj.author.image
+    });
+    let userName = $(`<p>${articleObj.author.username}</p>`).addClass("user-name");
+    let shortDate = articleObj.createdAt.slice(0, 10);
+    let postedOn = $(`<p>${shortDate}</p>`).addClass("posted-on");
+    let favoritesCount = articleObj.favoritesCount;
+    let articleTitle = $(`<h1>${articleObj.title}</h1>`);
+    let articleDescription = $(`<p>${articleObj.description}</p>`);
+    let readMore = $('<a/>', {
+        text: "Read more..."
+    }).prop("href", `https://demo.realworld.io/#/article/${articleObj.slug}`);
+
+    articleHTML.append(
+        '<hr>'
+    ).append(
+        $('<div/>', {
+            class: 'feed-container'
+        }).append(
+            $('<div/>').append(
+                userThumbnail
+            )).append(
+                $('<div/>').append(
+                    userName
+                ).append(
+                    postedOn
+                )).append(
+                    $('<div/>', {
+                        style: "text-align:right"
+                    }).append(
+                        $('<button/>', {
+                            type: "button",
+                            class: "btn btn-outline-success",
+                            text: `❤ ${favoritesCount}`
+                        }).click(function () {
+                            alert('Adding likes is not working jet!');
+                        })
+                    )
+                )
+    ).append(
+        $('<br>')
+    ).append(
+        articleTitle
+    ).append(
+        articleDescription
+    ).append(
+        $(`<span></span>`).append(
+            readMore
+        )
+    );
+
+    return articleHTML;
+};
 
